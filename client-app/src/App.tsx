c7fff7869f7f7f29f9afe0d439237492318e4a24
Jamison Miles <jamisonmiles234@gmail.com>
2021-10-28 11:10:47 -0500

    End of Section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+  const[activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, /*need to add array of dependencies to make sure that use effect only runs one time*/ []) 
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
+          {activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
+    </div>
+  );
+}
+
+export default App;
